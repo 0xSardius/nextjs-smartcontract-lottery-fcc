ab7436a0f0e995f544f87bc48c182330c796d09f
Sardius <0xsardius@gmail.com>
2023-04-13 14:29:14 -0400

    Added moralis components and hooks

--- components/ManualHeader.jsx
+++ components/ManualHeader.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import {useMoralis} from 'react-moralis'
 
 function ManualHeader() {
+  const {enableWeb3, account} = useMoralis()
+
+
   return (
-    <div>Hi From Header!</div>
+    <div>
+      <button onClick={async () => {await enableWeb3()}}>Connect</button>
+      
+    </div>
+    
   )
 }
 
-export default Header
+export default ManualHeader

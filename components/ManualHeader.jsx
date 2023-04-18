008a711bb07917c597e3903af9f4c37763429643
Sardius <0xsardius@gmail.com>
2023-04-17 20:13:33 -0400

    Adding hooks to ManualHeader

--- components/ManualHeader.jsx
+++ components/ManualHeader.jsx
@@ -1,17 +1,51 @@
-import React from 'react'
-import {useMoralis} from 'react-moralis'
+import { useEffect } from "react";
+import { useMoralis } from "react-moralis";
 
 function ManualHeader() {
-  const {enableWeb3, account} = useMoralis()
+  const { enableWeb3, account, isWeb3Enabled, Moralis, deactivateWeb3 } =
+    useMoralis();
 
+  useEffect(() => {
+    if (isWeb3Enabled) return;
+    if (typeof window !== "undefined") {
+      if (window.localStorage.getItem("connected") === "injected") {
+        enableWeb3();
+      }
+    }
+  }, [isWeb3Enabled]);
+  // no dependency array, so it will run on every render
+  // CAREFUL with this!! Because then you can get circular renders
+  // blank dependency array runs once on load
+
+  useEffect(() => {
+    Moralis.onAccountChanged((account) => {
+      console.log(`Account changed to ${account}`);
+      if (account == null) {
+        window.localStorage.removeItem("connected");
+        deactivateWeb3();
+        console.log("Null account found");
+      }
+    });
+  }, []);
 
   return (
     <div>
-      <button onClick={async () => {await enableWeb3()}}>Connect</button>
-      
+      {account ? (
+        <div>Connected to: {account}</div>
+      ) : (
+        <button
+          onClick={async () => {
+            await enableWeb3();
+            if (typeof window !== "undefined") {
+              window.localStorage.setItem("connected", "injected");
+            }
+          }}
+        >
+          Connect
+        </button>
+      )}
     </div>
-    
-  )
+  );
 }
 
-export default ManualHeader
+export default ManualHeader;

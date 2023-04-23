29101250ff562f4f0fb3f66a8789b750b5ad6441
Sardius <0xsardius@gmail.com>
2023-04-23 07:31:36 -0400

    Building out lottery entrance functions

--- components/LotteryEntrance.js
+++ components/LotteryEntrance.js
@@ -1,13 +1,35 @@
-import { useWeb3Contract } from "react-moralis";
+import { useWeb3Contract, useMoralis } from "react-moralis";
+import { abi, contractAddresses } from "../constants";
+import { useEffect } from "react";
 
 export default function LotteryEntrance() {
-  // const {runContractFunction: enterRaffle} = useWeb3Contract({
-  //     abi:
-  //     contractAddress:
-  //     functionName:
-  //     params: []
+  const { chainId: chainIdHex } = useMoralis();
+  console.log(parseInt(chainIdHex));
+  const chainId = parseInt(chainIdHex);
+  const raffleAddress =
+    chainIdHex in contractAddresses ? contractAddresses[chainId][0] : null;
+  //   const { runContractFunction: enterRaffle } = useWeb3Contract({
+  //     abi: abi,
+  //     contractAddress: raffleAddress,
+  //     functionName: "enterRaffle",
+  //     params: {},
+  //     msgValue:
+  //   });
 
-  // });
+  const { runContractFunction: getEntranceFee } = useWeb3Contract({
+    abi: abi,
+    contractAddress: raffleAddress,
+    functionName: "getEntranceFee",
+    params: {},
+  });
+
+  useEffect(() => {
+    if (isWeb3Enabled) {
+      async function updateUI() {
+        cons;
+      }
+    }
+  }, [isWeb3Enabled]);
   return (
     <div>
       <h1>Lottery Entrance</h1>
